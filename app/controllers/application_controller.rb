1f2100f837ae7443be711319a77eb80821df9f36
Kayla Ziegler <kaydanzie@gmail.com>
2019-04-15 14:58:02 -0700

    Requires login for every page except home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
+
+  # rubocop:disable Style/GuardClause
+  def require_login
+    if current_user.nil?
+      flash[:error] = "You must be logged in to view this resource."
+      redirect_to root_url
+    end
+  end
+  # rubocop:enable Style/GuardClause
 end
