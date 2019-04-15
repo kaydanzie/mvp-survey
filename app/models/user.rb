759a4e156db327c209cb2c3a713f62bbd7fef057
Kayla Ziegler <kaydanzie@gmail.com>
2019-04-15 15:29:03 -0700

    Adds devise user creation methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ApplicationRecord
-  devise :omniauthable, omniauth_providers: [:google_oauth2]
+  devise :database_authenticatable, :trackable, :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.from_omniauth(auth)
+    find_by(provider: auth.provider, uid: auth.uid) || create_from_auth(auth)
+  end
+
+  def self.create_from_auth(auth)
+    create(
+      provider: auth.provider,
+      uid: auth.uid,
+      first_name: auth.info.first_name,
+      last_name: auth.info.last_name,
+      email: auth.info.email
+    )
+  end
 end
