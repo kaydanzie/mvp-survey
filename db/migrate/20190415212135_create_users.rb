fbb71f3a850ff0bcfd0a4902ffd20db70c8f2fea
Kayla Ziegler <kaydanzie@gmail.com>
2019-04-15 14:30:55 -0700

    Create users table with devise

---
+++ db/migrate/20190415212135_create_users.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email,              null: false, default: ""
+      t.string :provider
+      t.string :uid
+      t.string :first_name
+      t.string :last_name
+      t.string :role
+
+      t.timestamps
+    end
+
+    add_index :users, :email,                unique: true
+  end
+end
