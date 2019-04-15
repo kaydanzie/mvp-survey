d97b74f157f1048deb0f1a0f5f771a8677743604
Kayla Ziegler <kaydanzie@gmail.com>
2019-04-15 15:31:58 -0700

    Add missing columns to user table

--- db/migrate/20190415223130_create_users.rb
+++ db/migrate/20190415223130_create_users.rb
@@ -3,12 +3,22 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      ## Database authenticatable
       t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
       t.string :provider
       t.string :uid
       t.string :first_name
       t.string :last_name
       t.string :role
+
+      ## Trackable
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.inet     :current_sign_in_ip
+      t.inet     :last_sign_in_ip
 
       t.timestamps
     end
