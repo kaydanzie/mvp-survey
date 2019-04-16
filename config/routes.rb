fb7a13036112739a4d2143baf0e627ac8294ad4e
Kayla Ziegler <kaydanzie@gmail.com>
2019-04-16 15:46:10 -0700

    Route for user updates

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  resources :users, only: :index
+  resources :users, only: [:index, :update]
 end
