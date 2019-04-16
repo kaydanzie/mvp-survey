a3519c2623d1a1eef08ab0d0b89a7ef1fb6e431a
Kayla Ziegler <kaydanzie@gmail.com>
2019-04-15 19:32:52 -0700

    Adds callback to set all FFI email address logins as employees

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :trackable, :omniauthable, omniauth_providers: [:google_oauth2]
 
+  after_create :initialize_role
+
+  def initialize_role
+    update(role: "employee") if ffi_email?
+  end
+
+  def ffi_email?
+    email.split('@')[1] == 'formulafolios.com'
+  end
+
   def self.from_omniauth(auth)
     find_by(provider: auth.provider, uid: auth.uid) || create_from_auth(auth)
   end
