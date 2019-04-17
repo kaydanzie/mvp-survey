03a3051248f7b681a42d7523df5205f6151e1e3d
Kayla Ziegler <kaydanzie@gmail.com>
2019-04-17 09:21:48 -0700

    Removes redundant config, already defined

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,9 +39,6 @@ RSpec.configure do |config|
   config.fixture_path = 'spec/factories'
   config.file_fixture_path = 'spec/factories/files'
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
