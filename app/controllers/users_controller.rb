89101c9f5d737b3383f3610dbf5752f671f178bc
Kayla Ziegler <kaydanzie@gmail.com>
2019-04-17 09:18:22 -0700

    Adds route comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   before_action :set_user, only: :update
 
+  # GET /users
   def index
     @users = User.order(:last_name)
   end
 
+  # PATCH/PUT /users/:id
   def update
     @user.update(user_params)
     success_msg = "#{@user.first_name}'s role successfully updated to #{@user.role.titleize}."
