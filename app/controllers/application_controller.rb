e6b8b8cb84b31b05acd9f113be94b3a449d130da
Kayla Ziegler <kaydanzie@gmail.com>
2019-04-22 16:16:26 -0700

    Rubocop violations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   def require_login
     return if current_user
+
     flash[:error] = "You must be logged in to view this resource."
     redirect_to root_url
   end
@@ -10,12 +11,14 @@ class ApplicationController < ActionController::Base
   # Make sure they have a valid user role
   def authorize_user
     return if User::ROLES.include?(current_user&.role)
+
     flash[:error] = "You must have a valid user role to view this resource."
     redirect_to root_url
   end
 
   def authorize_super_admin
     return if current_user&.super_admin?
+
     flash[:error] = "You must be an admin to view this resource."
     redirect_to root_url
   end
