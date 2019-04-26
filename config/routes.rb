a98cb9c9379fa85b020b849de43e2f9daf7137f0
Kayla Ziegler <kaydanzie@gmail.com>
2019-04-26 15:20:42 -0700

    Adding route for an edit page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  resources :users, only: [:index, :update]
+  resources :users, only: [:index, :update, :edit]
 end
