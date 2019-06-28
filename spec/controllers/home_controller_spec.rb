5aca90cbabf3d874f8d71cf1dd2ba0930095cffb
Kayla Ziegler <kaydanzie@gmail.com>
2019-06-27 21:48:24 -0700

    Moves home controller spec into user authorization request spec
    Kind of makes sense here

--- spec/controllers/home_controller_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe HomeController do
-  it 'renders index' do
-    get :index
-    expect(response).to be_successful
-  end
-end
--- spec/requests/user_authorization_spec.rb
+++ spec/requests/user_authorization_spec.rb
@@ -11,6 +11,13 @@ RSpec.describe "User authorization", type: :request do
       get users_url
       expect(flash[:error]).to match("must be logged in")
     end
+
+    # Anyone should be able to view the home page
+    it 'renders home page' do
+      get root_url
+      expect(response).to be_successful
+      expect(response.body).to include("Welcome")
+    end
   end
 
   context 'when not authorized' do
