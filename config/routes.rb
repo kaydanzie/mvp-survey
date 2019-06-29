a6c814ccffda9d9705b1e7da586cd591893355f9
Kayla Ziegler <kaydanzie@gmail.com>
2019-06-28 19:21:31 -0700

    Adds all crud routes for surveys

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resources :users, only: [:index, :update, :edit]
+  resources :surveys
 end
