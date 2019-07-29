15983e6a904cf9da9697de906e55ded28d61486a
Kayla Ziegler <kaydanzie@gmail.com>
2019-07-28 21:17:27 -0700

    Adds user ID to nomination params, renders survey form on error, fixes typo

--- app/controllers/nominations_controller.rb
+++ app/controllers/nominations_controller.rb
@@ -1,13 +1,14 @@
 class NominationsController < ApplicationController
   # POST /nominations
   def create
+    params[:nomination][:user_id] = current_user.id
     @nomination = Nomination.new(nomination_params)
 
     respond_to do |format|
-      if @survey.save
+      if @nomination.save
         format.html { redirect_to surveys_path, notice: 'Successfully submitted nomination.' }
       else
-        format.html { render :new }
+        format.html { render 'surveys/nominate' }
       end
     end
   end
