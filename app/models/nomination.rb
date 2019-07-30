72083d5c82cee72fb01c349db5c277d6122630bb
Kayla Ziegler <kaydanzie@gmail.com>
2019-07-30 12:01:59 -0700

    Adds relationship between nominee and users table so user can be grabbed from nominee_id

--- app/models/nomination.rb
+++ app/models/nomination.rb
@@ -1,6 +1,7 @@
 class Nomination < ApplicationRecord
   belongs_to :user
   belongs_to :survey
+  belongs_to :nominee, class_name: 'User'
 
   validates :nominee_id, presence: { message: 'You must choose an employee to nominate' }
 end
