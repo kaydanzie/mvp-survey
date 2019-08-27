ccbd930c176659f166c5e41d5c091ba2a7506648
Kayla Ziegler <kaydanzie@gmail.com>
2019-08-27 15:50:35 -0700

    Adds validation so users cannot vote more than once on a survey

--- app/models/nomination.rb
+++ app/models/nomination.rb
@@ -4,4 +4,6 @@ class Nomination < ApplicationRecord
   belongs_to :nominee, class_name: 'User'
 
   validates :nominee_id, presence: { message: 'You must choose an employee to nominate' }
+  # Users can only nominate once per survey
+  validates :survey_id, uniqueness: { scope: :user_id }
 end
