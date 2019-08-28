1b870e86d65fdbfc60e9588e6d89bf5938c0243f
Kayla Ziegler <kaydanzie@gmail.com>
2019-08-28 10:36:00 -0700

    Make sure users can't nominate themselves, at the model level

--- app/models/nomination.rb
+++ app/models/nomination.rb
@@ -3,7 +3,12 @@ class Nomination < ApplicationRecord
   belongs_to :survey
   belongs_to :nominee, class_name: 'User'
 
-  validates :nominee_id, presence: { message: 'You must choose an employee to nominate' }
   # Users can only nominate once per survey
   validates :survey_id, uniqueness: { scope: :user_id }
+  validates :nominee_id, presence: { message: 'You must choose an employee to nominate' }
+  validate :no_self_nominating
+
+  def no_self_nominating
+    errors.add(:nominee_id, "You can't nominate yourself!") if nominee_id == user_id
+  end
 end
