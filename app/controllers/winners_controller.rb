f460ad8faa344e4c5a33f26e1f6b5e1ceea6630d
Kayla Ziegler <kaydanzie@gmail.com>
2019-10-28 16:19:05 -0700

    Adds create action for winners

--- app/controllers/winners_controller.rb
+++ app/controllers/winners_controller.rb
@@ -1,9 +1,20 @@
-class Surveys::WinnersController < ApplicationController
+class WinnersController < ApplicationController
   load_and_authorize_resource
   before_action :set_survey
 
+  # POST /surveys/:survey_id/winners
   def create
+    @winner = Winner.new(winner_params)
 
+    respond_to do |format|
+      if @winner.save
+        format.html {
+          redirect_to surveys_path, notice: "Selected #{@winner.user.full_name} as MVP!"
+        }
+      else
+        format.html { render 'nominations/index' }
+      end
+    end
   end
 
   private
