4ed40630d37e64425b4cd3d8e4a12cc26be6280c
Kayla Ziegler <kaydanzie@gmail.com>
2019-10-29 18:56:48 -0700

    Decided the message about nominations being closed was redundant, show nothing

--- app/helpers/surveys_helper.rb
+++ app/helpers/surveys_helper.rb
@@ -7,7 +7,7 @@ module SurveysHelper
 
   def nomination_message(my_nomination, survey)
     return "You nominated #{my_nomination.nominee.full_name}" if my_nomination
-    return "Nominations are closed, a winner has been chosen!" if survey.winner
+    return if survey.winner
 
     voting_link = link_to "(Vote Here)", new_survey_nomination_path(survey)
     safe_join(["You haven't nominated anyone for this survey ", voting_link])
