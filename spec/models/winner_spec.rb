d28c8980b1287e151c46b38b1703e89a7798ea73
Kayla Ziegler <kaydanzie@gmail.com>
2020-03-04 10:55:25 -0500

    Adds spec to make sure an email is queued whenever a winner is created

--- spec/models/winner_spec.rb
+++ spec/models/winner_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe Winner do
     it { expect(winner_subject).to validate_uniqueness_of(:survey_id) }
     it { expect(winner_subject).to validate_presence_of(:user_id) }
   end
+
+  it 'sends WinnerEmail after creation' do
+    # Act & Assert
+    expect { create(:winner) }.to have_enqueued_job.on_queue('mailers')
+  end
 end
