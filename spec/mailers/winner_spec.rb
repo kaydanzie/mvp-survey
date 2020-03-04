63576901d91ec6ec54058a72d3423faa51f3bcd4
Kayla Ziegler <kaydanzie@gmail.com>
2020-03-04 11:05:49 -0500

    Tests the winner email's header and body

---
+++ spec/mailers/winner_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe WinnerMailer do
+  let(:winner) { create(:winner) }
+  let(:mail) { WinnerMailer.winner_email(winner.id, winner.survey.id) }
+
+  describe "#winner_email" do
+    it 'renders the body' do
+      expect(mail.body.encoded).to match("To see all past winners")
+    end
+
+    it 'renders the headers' do
+      expect(mail.subject).to match("New MVP")
+      expect(mail.to).to include(winner.user.email)
+    end
+  end
+end
